84aba83c6b945b3f6d11e7cbfd8129f379b1f78e
JazL0T <jazlizharfan@outlook.com>
2025-11-05 17:22:43 +0800

    Delete Fake News Detector 101 directory

--- Fake News Detector 101/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  const { fnd_privacyAccepted } = await chrome.storage.local.get("fnd_privacyAccepted");
-
-  if (!fnd_privacyAccepted) {
-    await chrome.storage.local.set({ fnd_privacyShown: true });
-    chrome.tabs.create({ url: chrome.runtime.getURL("privacy_policy.html") });
-  }
-});
